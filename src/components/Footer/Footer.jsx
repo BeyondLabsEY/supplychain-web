6c0d92b8da95a9922df331610d63184f49cb4652
Eduardo Basso <ebmoraesjr@gmail.com>
2019-04-30 17:47:02 -0300

    Update header and footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
 
 import "./Footer.scss";
-import Copyright from "../../assets/img/copyright.png";
 
 class Footer extends Component {
   render() {
     return (
       <footer>
-        <p className="sr-only" lang="en" translate="no">Created by BeyondLabsEY</p>
-        <img className="copyright" src={Copyright} alt="Frase de rodapé contando o aviso de direito autoral" />
+        <p lang="en" translate="no">Created by BeyondLabsEY</p>
       </footer>
     );
   }
